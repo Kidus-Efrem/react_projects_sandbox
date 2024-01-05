60e1cd9c274f10775618c466143897d329f96468
kidus <kidusefrem2@gmail.com>
2024-01-05 16:33:45 +0300

    add react icons

--- src/projects/03-like-my-photo/LikePhotoApp.jsx
+++ src/projects/03-like-my-photo/LikePhotoApp.jsx
@@ -1,23 +1,29 @@
 import React from 'react'
 import Title from '../components/Title'
+import {AiFillSmile, AiFillHeart, AiOutlineHeart,AiOutlineComment} from "react-icons/ai"
 import 'bootstrap/dist/css/bootstrap.min.css'
+import dogyDog from "./img/dog.webp"
 export default function LikePhotoApp() {
+	let liked=false;
   return (
 	<div className='container text-align'>
 	<Title text="Like photo App"/>
 	<Title className={"sub-header"} text="Likes"/>
 	<div
-	className='card card-dark m-auto'
+	className='card bg-dark text-white m-auto'
 	style={{width:300, cursor:"pointer"}}
 
 	>
-	<div className="card-header fs-xl">
+	<div className="card-head fs-3">
+		<AiFillSmile className='me-3'/>
 		<small>DogyDog</small>
 	</div>
-	<img src="" alt="img" />
-	<div className="card-footer fs-xl d-flex"
+	<img src={dogyDog} alt="img" />
+	<div className="card-footer fs-3 d-flex"
 style={{justifyContent:"space-between"}}
-	></div>
+	>
+		<AiOutlineComment />{liked?(<AiFillHeart className='text-danger'/>):<AiOutlineHeart/>}
+	</div>
 	</div>
 	</div>
   )
