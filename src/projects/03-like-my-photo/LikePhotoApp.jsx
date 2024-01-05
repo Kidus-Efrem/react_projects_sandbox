191b925179d677ccf66235596a027270fd842380
kidus <kidusefrem2@gmail.com>
2024-01-05 17:10:08 +0300

    add like on click and double click

--- src/projects/03-like-my-photo/LikePhotoApp.jsx
+++ src/projects/03-like-my-photo/LikePhotoApp.jsx
@@ -1,28 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Title from '../components/Title'
 import {AiFillSmile, AiFillHeart, AiOutlineHeart,AiOutlineComment} from "react-icons/ai"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import dogyDog from "./img/dog.webp"
 export default function LikePhotoApp() {
-	let liked=false;
+  const [like, setLike] =useState(false)
+  const [count, setCount] =useState(0)
+  function handleClick(event){
+	if(!like){
+		setLike(true)
+		setCount(count+1)
+	}
+	else{
+		setLike(false)
+		setCount(count-1)
+	}
+  }
   return (
 	<div className='container text-align'>
 	<Title text="Like photo App"/>
-	<Title className={"sub-header"} text="Likes"/>
+	<Title className={"sub-header"} text={`Likes ${count}`}/>
 	<div
-	className='card bg-dark text-white m-auto'
+	className='card shadow-lg bg-dark text-white m-auto'
 	style={{width:300, cursor:"pointer"}}
+	onDoubleClick={handleClick}
 
 	>
-	<div className="card-head fs-3">
+	<div className="card-head fs-1">
 		<AiFillSmile className='me-3'/>
 		<small>DogyDog</small>
 	</div>
 	<img src={dogyDog} alt="img" />
-	<div className="card-footer fs-3 d-flex"
+	<div className="card-footer fs-1 d-flex"
 style={{justifyContent:"space-between"}}
 	>
-		<AiOutlineComment />{liked?(<AiFillHeart className='text-danger'/>):<AiOutlineHeart/>}
+		<AiOutlineComment />{like?(<AiFillHeart onClick={handleClick} className='text-danger'/>):<AiOutlineHeart onClick={handleClick}/>}
 	</div>
 	</div>
 	</div>
