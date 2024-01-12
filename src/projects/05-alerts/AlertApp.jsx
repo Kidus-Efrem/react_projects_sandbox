cfdde901c844aa0e25118e3040d1efa6669ff073
kidus <kidusefrem2@gmail.com>
2024-01-12 11:21:11 +0300

    Automatically close alert with delay useing time trigger

--- src/projects/05-alerts/AlertApp.jsx
+++ src/projects/05-alerts/AlertApp.jsx
@@ -5,6 +5,7 @@ export default function AlertApp() {
   return (
 	<div className='container mx-0 my-3'>
 		<Alert type={"success"} message={"connected successfully"}/>
+		<Alert type={"danger"} delay='true' message={"not connected"} delayTime='3000'/>
 	</div>
   )
 }
--- src/projects/components/Alert.jsx
+++ src/projects/components/Alert.jsx
@@ -1,13 +1,20 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import Button from './Button';
-export default function Alert({type, message}) {
+export default function Alert({type, message,delay=false, delayTime=5000}) {
  const [showAlert, setShowAlert]=useState(true)
  function closeAlert(event){
-  event.target.parentElement.parentElement.classList.add()
+  event.target.parentElement.parentElement.classList.add("fade-out")
   setTimeout(()=>{
     setShowAlert(false)
   }, 400)
  }
+
+ useEffect(()=>
+ {
+  delay && setTimeout(()=>{
+    setShowAlert(false)
+  }, delayTime)
+ }, [delay, delayTime])
  return (
   showAlert && (
     <div>
