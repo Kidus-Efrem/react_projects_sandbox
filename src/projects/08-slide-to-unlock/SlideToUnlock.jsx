d9953952e4230bd6477c8f4fe3ca3533b34b65c5
kidus <kidusefrem2@gmail.com>
2024-02-17 00:59:20 +0300

    create the lock sceen state

--- src/projects/08-slide-to-unlock/SlideToUnlock.jsx
+++ src/projects/08-slide-to-unlock/SlideToUnlock.jsx
@@ -2,22 +2,30 @@ import React, { useState } from 'react';
 import LockSlider from './LockSlider';
 import { AiFillUnlock } from 'react-icons/ai';
 import Title from '../components/Title';
-
+import LockScreenImg from "./img/moon.jpeg"
+import HomeScreenImg from "./img/home.jpeg"
 export default function SlideToUnlock() {
+	const [uiProps, setUiProps]= useState({
+		uiText:"unlock screen",
+		uiColor: `#fff`,
+		uiBg: `url(${LockScreenImg})  center/cover`
+	})
     const [showLockSlider, setShowLockSlider] = useState(true);
     return (
         <div
-            className='container d-flex flex-column justify-content-center align-items-center text-center'
+            className='container  d-flex flex-column justify-content-center align-items-center text-center'
             style={{
                 height: '70vh',
                 marginTop: '15vh',
                 width: 340,
                 border: '4px solid #000',
                 borderRadius: 15,
+				background: uiProps.uiBg
+
 
             }}
         >
-            <Title></Title>
+            <Title text={uiProps.uiText} styles={{color: uiProps.uiColor}}></Title>
             {showLockSlider ? (
                 <LockSlider width={'250px'} />
             ) : (
--- src/projects/components/Title.jsx
+++ src/projects/components/Title.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default function Title({ text, classes }) {
+export default function Title({ text, classes, styles}) {
   const className = classes ? `header text-center ${classes}` : 'header text-center';
-
+  const style= styles? styles:""
   return (
     <div>
-      <h1 className={className}>{!text ? 'Title' : text}</h1>
+      <h1 className={className} style={styles}>{!text ? 'Title' : text}</h1>
     </div>
   );
 }
